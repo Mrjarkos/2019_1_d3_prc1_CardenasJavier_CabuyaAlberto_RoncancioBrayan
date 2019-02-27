ea2abc81f2a780549059bb18472f77e57eaf3efc
Javier Cardenas <javiercardenas@usantotomas.edu.co>
2019-02-27 08:48:09 -0500

    Modificacion de la clase BankAccount.cpp y BankAccount.h

--- BankAccount.cpp
+++ BankAccount.cpp
@@ -1,8 +1,90 @@
 
-class BankAccount
-{
-public:
-	BankAccount();
-	~BankAccount();
-	
-};
+#include "BankAccount.h" 
+
+class BankAccount(){
+
+	bool AccState;
+	char*  Key;
+ 	Balance balance;
+	char* Owner;
+
+public: 
+	int Accountnumber; 
+
+	BankAccount(int accountnumber, bool accstate, char* key, char* client, int initial_value){
+		Accountnumber= accountnumber;
+		AccState=accstate;
+		Key= key;
+		balance.Activos=initial_value;
+		balance.Debe=0;
+		balance.Saldo = balance.Activos-balance.Debe;
+		Owner= client;
+	} 
+
+	void Block(){
+		AccState= false;
+	}
+
+	void UnBlock(){
+		AccState= true;
+	}
+
+	bool CheckKey(char* Keyint){
+		char* Keyuser = &Key;
+		if(Keyuser == Keyint){
+			return true;
+		}
+		else{
+			return false;
+		}
+
+	}
+
+	void deposit(int moneyVal){
+		if(balance.deuda>0){
+			balance.deuda-= moneyVal;
+			if(balance.deuda<0){
+				balance.Activos= (-1)*(balance.deuda);
+			}
+		}
+		else{
+			balance.Activos+= moneyVal;
+		}			//suma al activo de la cuenta lo que se ingresa
+}
+
+	void Retirar(int moneyVal){				//Resta al activo y compara si hay deuda
+		balance.Activos-= moneyVal;
+		if(balance.Activos<=0){ 
+			balance.Debe+=(-1)*(balance.Activos);
+			balance.Activos=0;
+		 }
+	}
+
+	bool ConsultState(){
+		bool estado = AccState;
+		return estado;
+	}
+
+	Balance* ConsultBalance(){
+		Balance* Balanceclient = &balance;
+		Balanceclient*.Saldo = Balanceclient.Activos - Balanceclient.Debe; 
+		return Balanceclient;
+	}
+
+	char* ConsultUser(){
+		char* client = Owner;
+		return client;
+	}
+
+	void ChangeClient(BankClient* client){
+		Client= client;
+	}
+
+	void updateKey(char* accountkey){
+		char* newKey= accountkey;
+		AccState= newKey;
+	}
+
+
+
+}
--- BankAccount.h
+++ BankAccount.h
@@ -1,1 +1,38 @@
 
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+#include "BankClient.h"
+
+struct Balance
+ {
+ 	int Activos:
+ 	int Debe; 
+ 	int Saldo;
+ }; 
+
+class BankAccount(){
+
+	//Propiedades
+	int Accountnumber;
+	bool AccState; //true = activo, false = inactivo
+	char*  Key;
+	char* Owner;
+	Balance balance;
+
+	//Constructor
+	BankAccount(int accountnumber, bool accstate, char* key, char* Owner);
+
+	//Metodos
+ 	void Blockacc();
+ 	void UnBlockacc();
+ 	bool CheckKey(char* Keyint);
+ 	void deposit(int moneyVal);
+ 	void Retirar(int moneyVal);
+ 	void updateKey(char* accountkey);
+	bool ConsultState();
+	Balance* ConsultBalance();
+	char* ConsultUser();
+	void ChangeClient(BankClient* client);
+}
+
+#endif
